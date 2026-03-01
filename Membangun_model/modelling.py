ac4f1ba64dcc30db949ab84118d667250fba19d6
harianja961352 <almakky1397@gmail.com>
2026-03-01 17:50:10 +0700

    docs: update seluruh bukti screenshot submission untuk revisi

--- Membangun_model/modelling.py
+++ Membangun_model/modelling.py
@@ -5,6 +5,7 @@ import dagshub
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
+import joblib
 
 # 1. SETUP DAGSHUB & MLFLOW
 # GANTI link di bawah dengan link .mlflow yang kamu salin dari DagsHub
@@ -20,6 +21,11 @@ y = df['cnt']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+# --- KRITERIA 2: MENGAKTIFKAN AUTOLOG (WAJIB) ---
+# Mengaktifkan autolog sebelum proses training dimulai
+mlflow.sklearn.autolog()
+# ------------------------------------------------
+
 # 3. TRAINING MODEL DENGAN MLFLOW
 with mlflow.start_run():
     # Parameter model
@@ -27,6 +33,7 @@ with mlflow.start_run():
     max_depth = 10
     
     model = RandomForestRegressor(n_estimators=n_estimators, max_depth=max_depth, random_state=42)
+    # Begitu .fit() dipanggil, MLflow Autolog akan mencatat semuanya secara otomatis
     model.fit(X_train, y_train)
     
     # Prediksi & Evaluasi
@@ -34,18 +41,10 @@ with mlflow.start_run():
     rmse = mean_squared_error(y_test, predictions) ** 0.5
     r2 = r2_score(y_test, predictions)
     
-    # Log Parameter & Metric ke DagsHub
-    mlflow.log_param("n_estimators", n_estimators)
-    mlflow.log_param("max_depth", max_depth)
-    mlflow.log_metric("rmse", rmse)
-    mlflow.log_metric("r2", r2)
-    
-    # Simpan Model
-    mlflow.sklearn.log_model(model, "random_forest_model")
+
     
     print(f"Model berhasil dilatih! RMSE: {rmse:.2f}, R2: {r2:.2f}")
 
-import joblib
 # Menyimpan model ke folder Dashboard agar bisa dipakai Streamlit
 joblib.dump(model, '../Dashboard/bike_model.joblib')
 print("Model berhasil disimpan ke folder Dashboard!")
